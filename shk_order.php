4088f859b212d928816794280991db92596a2e1f
Denis A <artdevice@gmail.com>
2016-02-14 18:53:07 +0200

    Update shk_order.php

--- shk_order.php
+++ shk_order.php
@@ -1,10 +1,10 @@
 <?php
 
 /* Получаем информацию о заказе 550 */
- $order_id = 550;
- if ($order_id) {
-		$modx->addPackage('shopkeeper', MODX_CORE_PATH."components/shopkeeper/model/");
-		$order = $modx->getObject('SHKorder',array('id'=>$order_id));
-		$output = $order->toArray();
-		print_r($output);
-	} 
+$order_id = 550;
+if ($order_id) {
+	$modx->addPackage('shopkeeper', MODX_CORE_PATH."components/shopkeeper/model/");
+	$order = $modx->getObject('SHKorder',array('id'=>$order_id));
+	$output = $order->toArray();
+	print_r($output);
+} 
